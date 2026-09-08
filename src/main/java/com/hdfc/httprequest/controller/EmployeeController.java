27d8b4a0cb46adb95a361bf94deebb16ecdbb2e3
Karan <karan.l.k123321@gmail.com>
2026-09-08 20:21:31 +0530

    feat(http-request-journey): add request parameter handling

--- src/main/java/com/hdfc/httprequest/controller/EmployeeController.java
+++ src/main/java/com/hdfc/httprequest/controller/EmployeeController.java
@@ -2,33 +2,66 @@ package com.hdfc.httprequest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-	@GetMapping("/employees/{id}")
-	public String getEmployee(@PathVariable Integer id) {
+	/*
+	 * @GetMapping("/employees/{id}") public String getEmployee(@PathVariable
+	 * Integer id) {
+	 * 
+	 * return "empId: " + id;
+	 * 
+	 * }
+	 * 
+	 * @GetMapping("/employees/{id}/{name}") public String
+	 * getEmployeeDetails(@PathVariable Integer id, @PathVariable String name
+	 * 
+	 * ) {
+	 * 
+	 * return "Name: " + name + " Id: " + id;
+	 * 
+	 * }
+	 * 
+	 * @GetMapping("/emp/{id}") public String getEmpAgain(@PathVariable("id")
+	 * Integer empId) {
+	 * 
+	 * return "empId: " + empId;
+	 * 
+	 * // explicit mapping }
+	 */
 
-		return "empId: " + id;
+	@GetMapping("/employees")
+	public String getEmployees(@RequestParam String department) {
+
+		return "Department :" + department;
 
 	}
 
-	@GetMapping("/employees/{id}/{name}")
-	public String getEmployeeDetails(@PathVariable Integer id, @PathVariable String name
+	@GetMapping("/employeeDetails")
+	public String getEmployees(@RequestParam("department") String dept, @RequestParam(required = false) String city,
+			@RequestParam double salary) {
 
-	) {
+		return "Department: " + dept + " \nCity :" + city + " \nSalary :" + salary;
 
-		return "Name: " + name + " Id: " + id;
-
+		// all three query params are compulsory, else make it required=false
+		/*
+		 * Department: IT City :null Salary :58000.0
+		 */
 	}
 
-	@GetMapping("/emp/{id}")
-	public String getEmpAgain(@PathVariable("id") Integer empId) {
+	@GetMapping("/employeeDet")
+	public String getEmployees2(@RequestParam(defaultValue="All departments") String department) {
 
-		return "empId: " + empId;
-		
-		//explicit mapping
+		// optional query param
+		return "Department :" + department;
+
+		// output(department not given)---> null
+		// if department given---> IT
 	}
+	
+	
 
 }
